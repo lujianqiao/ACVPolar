012627797c1de53a2866156ae3b4f1ec5284e00f
baoxiangguo <baixiangguo@lnspt.com>
2025-08-07 14:30:40 +0800

    feat: creat project

---
+++ ACVPolar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ACVPolar
+//  
+//  Created by _.
+//  Copyright © 2025/8/7 _. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ACVPolarUITests/ACVPolarUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACVPolarUITestsLaunchTests.swift
+//  ACVPolarUITests
+//  
+//  Created by _.
+//  Copyright © 2025/8/7 _. All rights reserved.
+//
+
+import XCTest
+
+final class ACVPolarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
